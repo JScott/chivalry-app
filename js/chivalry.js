2b25c93df8aed18862cc4fc69b998bba60124ce9
JScott <jvscott@gmail.com>
2015-02-15 17:40:18 -0500

    Some cool looking preloading effects. helpful? dunno.

--- js/chivalry.js
+++ js/chivalry.js
@@ -2,11 +2,40 @@ function randomBetween(a, b) {
   return Math.floor((Math.random() * b) + a);
 }
 
+function preload(warcries) {
+  var loadingElement = document.getElementsByTagName('div')[0];
+  var totalAnimated = 67;
+  var totalMobile = 63;
+
+  function loadWarcries(type, index) {
+    var path = 'audio/'+type+'/'+index+'.ogg'
+    var warcry = new Audio(path);
+    warcry.addEventListener('canplaythrough', function() {
+      warcries.push(warcry);
+      var percentage = (warcries.length/(totalAnimated+totalMobile)*100).toFixed(2);
+      loadingElement.innerText = 'Audio Loading: '+percentage+'%...\nResponse times are better when the audio is fully loaded.';
+      if (percentage == 100) loadingElement.hidden = true;
+      
+      var buttonElement = document.getElementsByTagName('img')[0];
+      buttonElement.style.webkitFilter = 'grayscale('+(50-(percentage/2))+'%)';
+      buttonElement.style.filter = buttonElement.style.webkitFilter;
+      buttonElement.style.opacity = 0.5+(percentage/50);
+      console.log(buttonElement.style.filter);
+    }, false);
+  }
+
+  for (var i = 1; i <= totalAnimated; i++) {
+    loadWarcries('animated', i);
+  }
+  for (var i = 1; i <= totalMobile; i++) {
+    loadWarcries('mobile', i);
+  }
+}
+
 window.onload = function() {
   var width = window.screen.width;
   var height = window.screen.height;
   var buttonSize = Math.min(width,height)*0.6;
-  var loadingElement = document.getElementsByTagName('div')[0];
   var buttonElement = document.getElementsByTagName('img')[0];
 
   buttonElement.style.width = buttonSize+'px';
@@ -16,19 +45,7 @@ window.onload = function() {
   buttonElement.style.top = (height-buttonSize)/2+'px';
 
   var warcries = [];
-  //loadAudioTo(warcries);
-  for (var i = 1; i <= 67; i++) {
-    (function(index) {
-      var type = 'animated';
-      var path = 'audio/'+type+'/'+index+'.ogg'
-      var warcry = new Audio(path);
-      warcry.addEventListener('canplaythrough', function() {
-        warcries.push(warcry);
-        loadingElement.innerText = (warcries.length/67*100)+'%';
-        if (loadingElement.innerText == '100%') loadingElement.hidden = true;
-      }, false);
-    })(i);
-  }
+  preload(warcries);
 
   document.addEventListener("click", function() {
     var random = randomBetween(0,warcries.length-1);
